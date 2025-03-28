eafa8ab620321828579153e17c37d98d472c9382
Saima Eva <saimaislameva@gmail.com>
2025-03-28 11:58:51 +0800

    Fix: declare variable x properly

--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello again from Saima! 🚀")
-	fmt.Println("Testing GitHub connection 💻✅")
+	x = 5 // ❌ Error: x is undeclared
+	fmt.Println(x)
 }
